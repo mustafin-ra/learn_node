c46c7646f9d7c202a856863fd988f62dfb5cd720
Renat <mustafin.mra@gmail.com>
2025-02-21 21:31:16 +0300

    my commit2

--- project_00_ascii_art/index.js
+++ project_00_ascii_art/index.js
@@ -1,6 +1,5 @@
 // console.log("Hello, Project 00!");
-//Commit2
-
+//commit 2
 const art = require('ascii-art');//подключаем модуль через require
 art.font('Hello, NodeJS!', 'Doom', (err, renderredText) => {
     if (err) {
